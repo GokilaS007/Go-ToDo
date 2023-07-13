240ee37d30ad4ff450fa4f1972f97bd477086f51
Gokila Subramanian <gokila.subramanian.007@gmail.com>
2023-07-13 10:44:03 +0530

    Initial Commit

---
+++ Go-ToDo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Go-ToDo
+//
+//  Created by Gokila on 13/07/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
